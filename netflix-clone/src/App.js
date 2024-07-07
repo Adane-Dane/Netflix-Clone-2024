e22b80d2bd65d83eb1628471260b0c92145d4fc2
Adane-Dane <cocadane4@gmail.com>
2024-07-08 02:35:51 +0300

    header and footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Footer from './componenets/Footer/Footer';
+import Header from './componenets/Header/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Header />
+      <Footer />
     </div>
   );
 }
