38a066061d19b153e7af61d37fa2271f260a27b1
Adane-Dane <cocadane4@gmail.com>
2024-07-11 01:18:03 +0300

    row

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../../componenets/Header/Header'
 import Banner from '../../componenets/Banner/Banner'
 import Footer from '../../componenets/Footer/Footer'
+import RowList from '../../componenets/Rows/RowList/RowList'
 
 const Home = () => {
   return (
     <>
     <Header />
     <Banner />
+    <RowList />
     <Footer />
     </>
   )
